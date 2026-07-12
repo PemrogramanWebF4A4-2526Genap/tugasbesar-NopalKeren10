46bb73026fba75d798459240a2d40507caf48d7e
NopalKeren10 <202410715252@mhs.ubharajaya.ac.id>
2026-07-12 19:40:29 +0700

    Delete components directory

--- components/seller_footer.php
+++
@@ -1,6 +0,0 @@
-</main> <footer class="text-center text-xs text-gray-400 py-6 mt-auto border-t border-gray-200 w-full bg-gray-50">
-            <p>&copy; 2026 BookStore E-commerce. All Rights Reserved.</p>
-        </footer>
-
-    </div> </body>
-</html>
